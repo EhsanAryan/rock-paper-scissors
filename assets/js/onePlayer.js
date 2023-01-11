3cb22bfa1ed7adedd52837e109ef12d777ab3e6d
Ehsan <ehsan.aryan2001@gmail.com>
2023-01-11 12:27:00 +0330

    The first commit.

---
+++ assets/js/onePlayer.js
@@ -0,0 +1,174 @@
+document.addEventListener("DOMContentLoaded" , () => {
+    // Variables
+    let allRounds = 0;
+    let round = 1;
+    let userScore = 0;
+    let pcScore = 0;
+    let userChoice = null;
+    let pcChoice = null;
+    // 0 : rock , 1 : paper , 2 : scissors
+
+    // Elements
+    const setRoundBtn = document.getElementById("set-rounds-btn");
+    const roundInput = document.getElementById("round-input");
+    const roundModal = document.getElementById("round-modal");
+    const modalBox = document.getElementById("modal-box");
+    const userLastChoiceDiv = document.getElementById("user-last-choice");
+    const pcLastChoiceDiv = document.getElementById("pc-last-choice");
+    const userScoreDiv = document.getElementById("user-score-div");
+    const pcScoreDiv = document.getElementById("pc-score-div");
+    const chooseBtns = document.querySelectorAll(".choose-btn");
+    const rockBtn = document.getElementById("rock-btn");
+    const paperBtn = document.getElementById("paper-btn");
+    const scissorsBtn = document.getElementById("scissors-btn");
+    const finishModal = document.getElementById("finish-modal");
+    const finishModalBox = document.getElementById("finish-modal-box");
+    const resetBtn = document.getElementById("reset-btn");
+    const winnerSpan = document.getElementById("winner-span");
+    const finalUserScoreSpan = document.getElementById("final-user-score");
+    const finalPcScoreSpan = document.getElementById("final-pc-score");
+
+    // Functions
+    const handleSetRounds = () => {
+        const cnt = Number(roundInput.value);
+        if(cnt < 1) {
+            alert("Enter a number greater than 0!");
+        } else {
+            allRounds = cnt;
+            roundModal.classList.add("fade-modal");
+            modalBox.classList.add("fade-modal-box");
+            setTimeout(() => {
+                roundModal.classList.add("d-none");
+            } , 301);
+        }
+    }
+
+    setRoundBtn.addEventListener("click", handleSetRounds);
+
+    roundInput.addEventListener("keydown", (event) => {
+        if(event.key === "Enter") {
+            handleSetRounds();
+        }
+    });
+
+    const doNothing = () => {
+        return;
+    }
+
+    const handleSetScore = () => {
+        if(userChoice === pcChoice) {
+            doNothing();
+        }
+        else if(userChoice === 0) {
+            if(pcChoice === 1) {
+                pcScore += 1;
+                pcScoreDiv.innerHTML = pcScore.toString();
+            }
+            else {
+                userScore += 1;
+                userScoreDiv.innerHTML = userScore.toString();
+            }
+        }
+        else if(userChoice === 1) {
+            if(pcChoice === 0) {
+                userScore += 1;
+                userScoreDiv.innerHTML = userScore.toString();
+            }
+            else {
+                pcScore += 1;
+                pcScoreDiv.innerHTML = pcScore.toString();
+            }
+        }
+        else if(userChoice === 2) {
+            if(pcChoice === 0) {
+                pcScore += 1;
+                pcScoreDiv.innerHTML = pcScore.toString();
+            }
+            else {
+                userScore += 1;
+                userScoreDiv.innerHTML = userScore.toString();
+            }
+        }
+    }
+
+    const handleScoring = () => {
+        pcLastChoiceDiv.innerHTML = "<div class='spinner-border text-dark'><span class='visually-hidden'>Loading...</span></div>"
+        for(let b of chooseBtns) {
+            b.classList.add("no-pointer-events");
+        }
+        setTimeout(() => {
+            let randomNumber = Math.ceil(Math.random() * Math.random() * 256000) % 3;
+            console.log(randomNumber);
+            switch (randomNumber) {
+                case 0:
+                    pcChoice = 0; 
+                    pcLastChoiceDiv.innerHTML = "Rock";
+                    break;
+                case 1:
+                    pcChoice = 1; 
+                    pcLastChoiceDiv.innerHTML = "Paper";
+                    break;
+                case 2:
+                    pcChoice = 2; 
+                    pcLastChoiceDiv.innerHTML = "Scissors";
+                    break;
+            }
+            handleSetScore();
+            round += 1;
+            for(let b of chooseBtns) {
+                b.classList.remove("no-pointer-events");
+            }
+            if(round > allRounds) {
+                winnerSpan.innerHTML = (userScore > pcScore) ? "You" : (userScore === pcScore) ? "Draw!" : "PC";
+                finalUserScoreSpan.innerHTML = userScore.toString();
+                finalPcScoreSpan.innerHTML = pcScore.toString();
+                finishModal.classList.remove("d-none");
+                setTimeout(() => {
+                    finishModal.classList.add("show-modal");
+                    finishModalBox.classList.add("show-modal-box");
+                }, 10);
+            }
+        }, 500);
+    }
+
+    rockBtn.addEventListener("click", () => {
+        userLastChoiceDiv.innerHTML = "Rock";
+        userChoice = 0;
+        handleScoring();
+    });
+
+    paperBtn.addEventListener("click", () => {
+        userLastChoiceDiv.innerHTML = "Paper";
+        userChoice = 1;
+        handleScoring();
+    });
+
+    scissorsBtn.addEventListener("click", () => {
+        userLastChoiceDiv.innerHTML = "Scissors";
+        userChoice = 2;
+        handleScoring();
+    });
+
+    resetBtn.addEventListener("click", () => {
+        allRounds = 0;
+        round = 1;
+        userScore = 0;
+        pcScore = 0;
+        userChoice = null;
+        pcChoice = null;
+        userLastChoiceDiv.innerHTML = "-";
+        pcLastChoiceDiv.innerHTML = "-";
+        userScoreDiv.innerHTML = "0";
+        pcScoreDiv.innerHTML = "0";
+        finishModal.classList.remove("show-modal");
+        finishModalBox.classList.remove("show-modal-box");
+        setTimeout(() => {
+            finishModal.classList.add("d-none");
+            roundModal.classList.remove("d-none");
+        }, 301);
+        setTimeout(() => {
+            roundModal.classList.remove("fade-modal");
+            modalBox.classList.remove("fade-modal-box");
+        }, 310);
+    });
+});
---
+++ assets/js/twoPlayers.js
@@ -0,0 +1,201 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Variables
+    let allRounds = 0;
+    let round = 1;
+    let user1Score = 0;
+    let user2Score = 0;
+    let user1Choice = null;
+    let user2Choice = null;
+    // 0 : rock , 1 : paper , 2 : scissors
+
+    // Elements
+    const setRoundBtn = document.getElementById("set-rounds-btn");
+    const roundInput = document.getElementById("round-input");
+    const roundModal = document.getElementById("round-modal");
+    const modalBox = document.getElementById("modal-box");
+    const user1LastChoiceDiv = document.getElementById("user1-last-choice");
+    const user2LastChoiceDiv = document.getElementById("user2-last-choice");
+    const user1ScoreDiv = document.getElementById("user1-score-div");
+    const user2ScoreDiv = document.getElementById("user2-score-div");
+    const user1Btns = document.querySelectorAll(".user1-btn");
+    const user2Btns = document.querySelectorAll(".user2-btn");
+    const rock1Btn = document.getElementById("rock-btn1");
+    const paper1Btn = document.getElementById("paper-btn1");
+    const scissors1Btn = document.getElementById("scissors-btn1");
+    const rock2Btn = document.getElementById("rock-btn2");
+    const paper2Btn = document.getElementById("paper-btn2");
+    const scissors2Btn = document.getElementById("scissors-btn2");
+    const finishModal = document.getElementById("finish-modal");
+    const finishModalBox = document.getElementById("finish-modal-box");
+    const resetBtn = document.getElementById("reset-btn");
+    const winnerSpan = document.getElementById("winner-span");
+    const finalUser1ScoreSpan = document.getElementById("final-user1-score");
+    const finalUser2ScoreSpan = document.getElementById("final-user2-score");
+
+
+    // Functions
+    const handleSetRounds = () => {
+        const cnt = Number(roundInput.value);
+        if(cnt < 1) {
+            alert("Enter a number greater than 0!");
+        } else {
+            allRounds = cnt;
+            roundModal.classList.add("fade-modal");
+            modalBox.classList.add("fade-modal-box");
+            setTimeout(() => {
+                roundModal.classList.add("d-none");
+            } , 301);
+        }
+    }
+
+    setRoundBtn.addEventListener("click", handleSetRounds);
+
+    roundInput.addEventListener("keydown", (event) => {
+        if(event.key === "Enter") {
+            handleSetRounds();
+        }
+    });
+
+    const doNothing = () => {
+        return;
+    }
+
+    const handleSetScore = () => {
+        if(user1Choice === user2Choice) {
+            doNothing();
+        }
+        else if(user1Choice === 0) {
+            if(user2Choice === 1) {
+                user2Score += 1;
+                user2ScoreDiv.innerHTML = user2Score.toString();
+            }
+            else {
+                user1Score += 1;
+                user1ScoreDiv.innerHTML = user1Score.toString();
+            }
+        }
+        else if(user1Choice === 1) {
+            if(user2Choice === 0) {
+                user1Score += 1;
+                user1ScoreDiv.innerHTML = user1Score.toString();
+            }
+            else {
+                user2Score += 1;
+                user2ScoreDiv.innerHTML = user2Score.toString();
+            }
+        }
+        else if(user1Choice === 2) {
+            if(user2Choice === 0) {
+                user2Score += 1;
+                user2ScoreDiv.innerHTML = user2Score.toString();
+            }
+            else {
+                user1Score += 1;
+                user1ScoreDiv.innerHTML = user1Score.toString();
+            }
+        }
+        round += 1;
+        if(round > allRounds) {
+            winnerSpan.innerHTML = (user1Score > user2Score) ? "Player 1" : (user1Score === user2Score) ? "Draw!" : "Player 2";
+            finalUser1ScoreSpan.innerHTML = user1Score.toString();
+            finalUser2ScoreSpan.innerHTML = user2Score.toString();
+            finishModal.classList.remove("d-none");
+            setTimeout(() => {
+                finishModal.classList.add("show-modal");
+                finishModalBox.classList.add("show-modal-box");
+            }, 10);
+        }
+    }
+
+    rock1Btn.addEventListener("click", () => {
+        user1Choice = 0;
+        user1LastChoiceDiv.innerHTML = "Rock";
+        for(let b of user1Btns) {
+            b.classList.add("no-pointer-events");
+        }
+        for(let b of user2Btns) {
+            b.classList.remove("no-pointer-events");
+        }
+    });
+
+    paper1Btn.addEventListener("click", () => {
+        user1Choice = 1;
+        user1LastChoiceDiv.innerHTML = "Paper";
+        for(let b of user1Btns) {
+            b.classList.add("no-pointer-events")
+        }
+        for(let b of user2Btns) {
+            b.classList.remove("no-pointer-events")
+        }
+    });
+
+    scissors1Btn.addEventListener("click", () => {
+        user1Choice = 2;
+        user1LastChoiceDiv.innerHTML = "Scissors";
+        for(let b of user1Btns) {
+            b.classList.add("no-pointer-events")
+        }
+        for(let b of user2Btns) {
+            b.classList.remove("no-pointer-events")
+        }
+    });
+
+    rock2Btn.addEventListener("click", () => {
+        user2Choice = 0;
+        user2LastChoiceDiv.innerHTML = "Rock";
+        handleSetScore();
+        for(let b of user2Btns) {
+            b.classList.add("no-pointer-events")
+        }
+        for(let b of user1Btns) {
+            b.classList.remove("no-pointer-events")
+        }
+    });
+
+    paper2Btn.addEventListener("click", () => {
+        user2Choice = 1;
+        user2LastChoiceDiv.innerHTML = "Paper";
+        handleSetScore();
+        for(let b of user2Btns) {
+            b.classList.add("no-pointer-events")
+        }
+        for(let b of user1Btns) {
+            b.classList.remove("no-pointer-events")
+        }
+    });
+
+    scissors2Btn.addEventListener("click", () => {
+        user2Choice = 2;
+        user2LastChoiceDiv.innerHTML = "Scissors";
+        handleSetScore();
+        for(let b of user2Btns) {
+            b.classList.add("no-pointer-events")
+        }
+        for(let b of user1Btns) {
+            b.classList.remove("no-pointer-events")
+        }
+    });
+
+    resetBtn.addEventListener("click", () => {
+        allRounds = 0;
+        round = 1;
+        user1Score = 0;
+        user2Score = 0;
+        user1Choice = null;
+        user2Choice = null;
+        user1LastChoiceDiv.innerHTML = "-";
+        user2LastChoiceDiv.innerHTML = "-";
+        user1ScoreDiv.innerHTML = "0";
+        user2ScoreDiv.innerHTML = "0";
+        finishModal.classList.remove("show-modal");
+        finishModalBox.classList.remove("show-modal-box");
+        setTimeout(() => {
+            finishModal.classList.add("d-none");
+            roundModal.classList.remove("d-none");
+        }, 301);
+        setTimeout(() => {
+            roundModal.classList.remove("fade-modal");
+            modalBox.classList.remove("fade-modal-box");
+        }, 311);
+    });
+});
